1cbcfdbce3f151e3b1520fb4281c949db0fdc1c4
gtj1227 <gtj@njit.edu>
2025-04-02 15:25:05 -0400

    Fixed up code

--- primewell-clinic-frontend/src/pages/PharmacistPortal/PillPage.jsx
+++ primewell-clinic-frontend/src/pages/PharmacistPortal/PillPage.jsx
@@ -1,42 +1,12 @@
 import {Flex, Input} from "antd"
 import { useEffect, useState } from "react"
 import axios from "axios"
-import PillInfo from "../../components/PillInfo"
 import PillFilter from "../../components/PillFilter"
 const PillPage = () => {
-    const [pillsInfo, setPillsInfo] = useState([])
-    const [reviewInfo, setReviewInfo] = useState([])
-    const [searchedPill, setSearchedPill] = useState("")
-    const [filteredInfo, setFilteredInfo] = useState([])
-
-    const fetchPillsInfo = async () => {
-        const res = await axios.get("http://localhost:3000/pillbank")
-        setReviewInfo(res.data)
-    }
-    
-        const handleSearch = (e) => {
-            setSearchedPill(e.target.value)
-        }
-    
-        const filteredPill = reviewInfo.filter((review) => {
-            if ( searchedPill === "" ) return reviewInfo
-             
-            return review.first_name.toLowerCase().includes(searchedPill.toLowerCase())
-        })
-    
-        useEffect(() => {
-            setFilteredInfo(filteredPill)
-        }, [searchedPill])
-
-        useEffect(() => {
-            fetchPillsInfo()
-            console.log(pillsInfo)
-        }, [])
-
     return (
         <Flex vertical justify="start" align="center" gap="60px" style={{
-            background: "#ffffff", 
             borderRadius: "12px",
+            backgroundColor: "#ffffff",
             padding: "33px 40px",
             width: "100%",
             overflowY: "auto"
@@ -45,7 +15,7 @@ const PillPage = () => {
                 justify="space-between" 
                 align="center" 
                 style={{ 
-                    backgroundColor: "#000000", 
+                    backgroundColor: "#ffffff", 
                     textAlign: "center",
                     width:'100%'}}>
                     <Flex
@@ -53,13 +23,13 @@ const PillPage = () => {
                     justify="start"
                     align="start"
                     style={{ 
-                        background: "#A8C4A2", 
-                        padding: "20px 30px", 
+                        background: "#fffff", 
                         width: "100%", 
+                        marginBottom: "-20px"
                     }}
                     >
-                       <h2>Pill Page</h2> 
-                       <div style={{marginLeft: "20px"}}>CVS</div>
+                       <h2 style={{fontFamily: "Poppins", fontSize: "22px", color: "#000000"}}>Pill Page</h2> 
+                       <div style={{marginLeft: "20px", fontFamily: "Poppins", fontSize: "14px", color: "#F09C96"}}>CVS</div>
                     </Flex>
             </Flex>
             <Flex vertical gap="20px" style={{
