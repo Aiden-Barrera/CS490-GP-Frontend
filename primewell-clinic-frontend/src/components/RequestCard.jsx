d60335757c360eea343f09e6f58ad0a0ad61ce7a
Aiden Barrera <aidenbarrera@macbookpro.resource.campus.njit.edu>
2025-04-07 16:49:17 -0400

    Added Calender to Request Card, not finished with implementation

--- primewell-clinic-frontend/src/components/RequestCard.jsx
+++ primewell-clinic-frontend/src/components/RequestCard.jsx
@@ -1,9 +1,18 @@
-import {Button, Flex} from "antd"
+import {Button, Flex, Calendar} from "antd"
+import { useState } from "react"
+import dayjs from 'dayjs';
 
 const RequestCard = (props) => {
+    const [btnClicked, setBtnClicked] = useState(false)
+    const [selectDate, setSelectDate] = useState(() => dayjs('2017-01-25'))
+
+    const handleSelect = (value) => {
+        setSelectDate(value)
+    }
+
     
     return (
-        <Flex align="center" justify="space-between" style={{
+        <Flex vertical gap="10px" style={{
             background: "#f09c96", 
             padding: "20px 30px", 
             width: "100%", 
@@ -12,36 +21,49 @@ const RequestCard = (props) => {
             boxSizing: "border-box",
             boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"
         }}>
-            <Flex>
-                <Flex gap="10px">
-                    <img src="/firstAidIcon.svg" alt="Icon" style={{ 
-                        width: "52px", 
-                        height: "auto", 
-                        borderRadius: "10px",
-                        flexShrink: 0
-                    }} />
-                    <h2 style={{color: "#ffffff", fontSize: "32px", borderRight: "3px solid #ffffff", paddingRight: "18px", margin: 0}}>Dr. {`${props.info.first_name} ${props.info.last_name}`}</h2>
-                    <h2 style={{color: "#ffffff", fontSize: "32px", borderRight: "3px solid #ffffff", paddingLeft: "9px", paddingRight: "18px", margin: 0}}>{`${props.info.specialty}`}</h2>
-                    <h2 style={{color: "#ffffff", fontSize: "32px", paddingLeft: "9px", margin: 0}}>{props.info.availability === 1 ? "Available" : "Not Available"}</h2>
+            {/* Section for Displaying Doctor Info and Availablilty */}
+            <Flex align="center" justify="space-between">
+                <Flex>
+                    <Flex gap="10px">
+                        <img src="/firstAidIcon.svg" alt="Icon" style={{ 
+                            width: "52px", 
+                            height: "auto", 
+                            borderRadius: "10px",
+                            flexShrink: 0
+                        }} />
+                        <h2 style={{color: "#ffffff", fontSize: "32px", borderRight: "3px solid #ffffff", paddingRight: "18px", margin: 0}}>Dr. {`${props.info.first_name} ${props.info.last_name}`}</h2>
+                        <h2 style={{color: "#ffffff", fontSize: "32px", borderRight: "3px solid #ffffff", paddingLeft: "9px", paddingRight: "18px", margin: 0}}>{`${props.info.specialty}`}</h2>
+                        <h2 style={{color: "#ffffff", fontSize: "32px", paddingLeft: "9px", margin: 0}}>{props.info.availability === 1 ? "Available" : "Not Available"}</h2>
+                    </Flex>
                 </Flex>
+                {props.info.availability === 1 ? (
+                    <Button type="primary" style={{
+                        borderRadius: "40%",  // Fully circular shape
+                        width: "50px",        // Ensures the button remains a circle
+                        height: "50px",
+                        display: "flex",      // Centers the image inside
+                        alignItems: "center",
+                        justifyContent: "center",
+                        background: "#ffe6e2",
+                        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"
+                    }} onClick={() => setBtnClicked(!btnClicked)}>
+                        <img src="/downArrow.svg" alt="Icon" style={{
+                            width: "32px", // Ensures circular shape
+                            height: "32px",
+                        }} />
+                    </Button>
+                ) : null}
             </Flex>
-            {props.info.availability === 1 ? (
-                <Button type="primary" style={{
-                    borderRadius: "40%",  // Fully circular shape
-                    width: "50px",        // Ensures the button remains a circle
-                    height: "50px",
-                    display: "flex",      // Centers the image inside
-                    alignItems: "center",
-                    justifyContent: "center",
-                    background: "#ffe6e2",
-                    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"
-                }}>
-                    <img src="/downArrow.svg" alt="Icon" style={{
-                        width: "32px", // Ensures circular shape
-                        height: "32px",
-                    }} />
-                </Button>
-            ) : null}
+
+            {/* Section for Displaying Sending Request */}
+            {btnClicked && (
+                <Flex style={{backgroundColor: "#ffe6e2"}}>
+                    <Calendar fullscreen={false} onSelect={handleSelect} style={{width: "300px", border: "1px solid #666666", borderRadius: "9px"}} />
+                    <Flex>
+                        {selectDate.format('YYYY-MM-DD')}
+                    </Flex>
+                </Flex>
+            )}
         </Flex>
     )
 }
