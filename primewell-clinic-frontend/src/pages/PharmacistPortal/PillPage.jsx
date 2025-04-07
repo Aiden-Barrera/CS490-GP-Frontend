f753e5770b1cd7fc87329804460884612e440c6a
gtj1227 <gtj@njit.edu>
2025-04-07 18:58:16 -0400

    Properly display pharmacy name

--- primewell-clinic-frontend/src/pages/PharmacistPortal/PillPage.jsx
+++ primewell-clinic-frontend/src/pages/PharmacistPortal/PillPage.jsx
@@ -1,6 +1,22 @@
 import {Flex, Input} from "antd"
+import { useEffect, useState } from "react"
 import PillFilter from "../../components/PillFilter"
+import axios from "axios"
 const PillPage = () => {
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000/pharmacies"); // update with your actual API endpoint
+                setUserInfo(response.data);
+            } catch (error) {
+                console.error("Failed to fetch user info:", error);
+            } 
+        };
+
+        fetchUserInfo();
+    }, []);
     return (
         <Flex vertical justify="start" align="center" gap="60px" style={{
             borderRadius: "12px",
@@ -27,7 +43,7 @@ const PillPage = () => {
                     }}
                     >
                        <h2 style={{fontFamily: "Poppins", fontSize: "22px", color: "#000000"}}>Pill Page</h2> 
-                       <div style={{marginLeft: "20px", marginTop: "-10px", fontFamily: "Poppins", fontSize: "14px", color: "#F09C96"}}>CVS</div>
+                       <div style={{marginTop: "-10px", fontFamily: "Poppins", fontSize: "14px", color: "#F09C96"}}> {userInfo?.[0]?.Company_Name} </div>
                     </Flex>
             </Flex>
             <Flex vertical gap="20px" style={{
