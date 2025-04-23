f26c954fae207ea041e6efd103259869ce7889f5
Aiden Barrera <aidenbarrera@macbookpro.resource.campus.njit.edu>
2025-04-23 18:47:37 -0400

    Added Feature to AppointmentChannel for when a doctor ends the appointment, to give the patient their feedback

--- primewell-clinic-frontend/src/pages/ApptChannel.jsx
+++ primewell-clinic-frontend/src/pages/ApptChannel.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState,  } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import io from "socket.io-client";
-import { Flex, Input, Button } from "antd";
+import { Flex, Input, Button, Popover, Modal, Form, Checkbox, message } from "antd";
 import axios from "axios";
 import dayjs from "dayjs";
-
-const socket = io.connect("http://localhost:3000");
+import socket from "../Socket";
 
 const ApptChannel = ({userInfo}) => {
   const [message, setMessage] = useState("");
@@ -13,7 +12,10 @@ const ApptChannel = ({userInfo}) => {
   const [appt_id, setAppt_ID] = useState("")
   const navigate = useNavigate()
   const location = useLocation()
-  const [senderName, setSenderName] = useState("")
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [form] = Form.useForm();
+  const [symptoms, setSymptoms] = useState({});
+  const [patientID, setPatientID] = useState("")
 
   const fetchMessages = async () => {
     const body = {
@@ -28,6 +30,7 @@ const ApptChannel = ({userInfo}) => {
 
   useEffect(() => {
     if (location.state?.appt_id) {
+      socket.connect()
       setAppt_ID(location.state.appt_id)
       console.log("Joining room:", location.state.appt_id); // <- check this
       fetchMessages()
@@ -44,6 +47,13 @@ const ApptChannel = ({userInfo}) => {
       };
     }
   }, [location.state]);
+
+  useEffect(() => {
+    const patientMessage = chatLog.find(msg => msg.senderType === "Patient");
+    if (patientMessage) {
+      setPatientID(patientMessage.senderID);
+    }
+  }, [chatLog])
 
   const sendMessage = () => {
     const messageData = {
@@ -64,65 +74,283 @@ const ApptChannel = ({userInfo}) => {
       Doctor_ID: userInfo.doctor_id
     }
     await axios.patch("http://localhost:3000/endAppointment", body)
-    navigate("/DoctorPortal/")
+    navigate("/DoctorPortal/DoctorFeedback", {
+      state: {
+        patient_id: patientID,
+        appt_id: appt_id
+      }
+    })
     
   }
 
+  const openModal = async () => {
+    const res = await axios.get(`http://localhost:3000/preliminaries/${patientID}`)
+    console.log("Preliminary Data: ", res.data)
+    const patientData = res.data[0]; // since the data is in an array
+    setSymptoms(patientData?.Symptoms || {});
+    setIsModalOpen(true)
+  }
+
+  const content = (
+    <div>
+      <Button type="primary" style={{fontWeight: "700", fontSize: "24px", backgroundColor: "#ffe6e2", color: "#333333", padding: "20px", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"}} onClick={openModal}>Patient Forum</Button>
+    </div>
+  )
+
   return (
-    <div style={{display: "flex", flexDirection: "column", width: "100%", backgroundColor: "#ffffff", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)", borderRadius: "12px", padding: "33px 40px"}}>
-      <h2>Room: {appt_id}</h2>
-      <div style={{ overflowY: "scroll", width: "100%", height: "90%", display: "flex", flexDirection: "column", justifyContent: "flex-start", gap: "10px", marginTop: "20px" }}>
-        {chatLog.map((msg, index) => {
-          const isEven = index % 2 === 0;
-          return (
-            <div
-              key={index}
-              style={{
-                display: "flex",
-                justifyContent: msg.senderType === "Patient" ? "flex-start" : "flex-end",
-                width: "100%",
-              }}
-            >
-              <div style={{ display: "flex", flexDirection: msg.senderType === "Patient" ? "row" : "row-reverse", alignItems: "flex-end", gap: "10px" }}>
-                <img
-                  src={msg.senderType === "Patient" ? `https://ui-avatars.com/api/?name=${encodeURIComponent(msg.senderName)}&background=random&color=fff&size=42` : `https://ui-avatars.com/api/?name=${encodeURIComponent(msg.senderName)}&background=random&color=fff&size=42`}
-                  alt="Message Icon"
-                  style={{
-                    width: "42px",
-                    height: "auto",
-                    borderRadius: "50%",
-                    objectFit: "cover",
-                  }}
-                />
-                <div
-                  style={{
-                    backgroundColor: msg.senderType === "Patient" ? "#f0f0f0" : "#d9f7be",
-                    borderRadius: "12px",
-                    padding: msg.senderType === "Patient" ? "10px 10px 10px 20px" : "10px 20px 10px 10px",
-                    width: "400px",
-                  }}
-                >
-                  <p style={{ margin: 0, fontSize: "24px", textAlign: msg.senderType === "Patient" ? "left" : "right" }}>{msg.message}</p>
-                  <p style={{ fontSize: "11px", margin: 0, textAlign: msg.senderType === "Patient" ? "left" : "right" }}>
-                    <strong>{dayjs(msg.sent_at).format("h:mm A")}</strong>
-                  </p>
+    <>
+
+      <div style={{display: "flex", flexDirection: "column", width: "100%", backgroundColor: "#ffffff", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)", borderRadius: "12px", padding: "33px 40px"}}>
+        <h2>Room: {appt_id}</h2>
+        <div style={{ overflowY: "scroll", width: "100%", height: "90%", display: "flex", flexDirection: "column", justifyContent: "flex-start", gap: "10px", marginTop: "20px" }}>
+          {chatLog.map((msg, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  display: "flex",
+                  justifyContent: msg.senderType === "Patient" ? "flex-start" : "flex-end",
+                  width: "100%",
+                }}
+              >
+                <div style={{ display: "flex", flexDirection: msg.senderType === "Patient" ? "row" : "row-reverse", alignItems: "flex-end", gap: "10px" }}>
+                  {msg.senderType === "Patient" ? (
+                    <Popover content={content} title="View Patient Preliminary Form">
+                      <img
+                        src={msg.senderType === "Patient" ? `https://ui-avatars.com/api/?name=${encodeURIComponent(msg.senderName)}&background=random&color=fff&size=42` : `https://ui-avatars.com/api/?name=${encodeURIComponent(msg.senderName)}&background=random&color=fff&size=42`}
+                        alt="Message Icon"
+                        title={msg.senderName}
+                        style={{
+                          width: "42px",
+                          height: "auto",
+                          borderRadius: "50%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </Popover>
+                  ) : (
+                      <img
+                        src={msg.senderType === "Patient" ? `https://ui-avatars.com/api/?name=${encodeURIComponent(msg.senderName)}&background=random&color=fff&size=42` : `https://ui-avatars.com/api/?name=${encodeURIComponent(msg.senderName)}&background=random&color=fff&size=42`}
+                        alt="Message Icon"
+                        title={msg.senderName}
+                        style={{
+                          width: "42px",
+                          height: "auto",
+                          borderRadius: "50%",
+                          objectFit: "cover",
+                        }}
+                      />
+
+                  )}
+                  <div
+                    style={{
+                      backgroundColor: msg.senderType === "Patient" ? "#f0f0f0" : "#d9f7be",
+                      borderRadius: "12px",
+                      padding: msg.senderType === "Patient" ? "10px 10px 10px 20px" : "10px 20px 10px 10px",
+                      width: "400px",
+                    }}
+                  >
+                    <p style={{ margin: 0, fontSize: "24px", textAlign: msg.senderType === "Patient" ? "left" : "right" }}>{msg.message}</p>
+                    <p style={{ fontSize: "11px", margin: 0, textAlign: msg.senderType === "Patient" ? "left" : "right" }}>
+                      <strong>{dayjs(msg.sent_at).format("h:mm A")}</strong>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <Flex justify="center" align="center" gap="15px" style={{marginTop: "30px"}}>
+          <Input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Type your message"
+            style={{fontSize: "24px", width: "700px", }}
+          />
+          <Button type="primary" style={{fontWeight: "700", fontSize: "24px", backgroundColor: "#ffe6e2", color: "#333333", padding: "20px", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"}}  onClick={sendMessage}>Send</Button>
+          {userInfo?.doctor_id && (<Button type="primary" style={{fontWeight: "700", fontSize: "24px", backgroundColor: "rgb(239, 71, 111)", color: "#ffffff", padding: "20px", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"}} onClick={endAppointment}>End Appointment</Button>)}
+        </Flex>
       </div>
-      <Flex justify="center" align="center" gap="15px" style={{marginTop: "30px"}}>
-        <Input
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type your message"
-          style={{fontSize: "24px", width: "700px", }}
-        />
-        <Button type="primary" style={{fontWeight: "700", fontSize: "24px", backgroundColor: "#ffe6e2", color: "#333333", padding: "20px", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"}}  onClick={sendMessage}>Send</Button>
-        {userInfo?.doctor_id && (<Button type="primary" style={{fontWeight: "700", fontSize: "24px", backgroundColor: "rgb(239, 71, 111)", color: "#ffffff", padding: "20px", boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)"}} onClick={endAppointment}>End Appointment</Button>)}
-      </Flex>
-    </div>
+
+      <Modal open={isModalOpen} onCancel={() => setIsModalOpen(false)} footer={false} centered className="style-modal" width={650}>
+        <Flex
+          vertical
+          justify="center"
+          align="center"
+          style={{
+            border: "1px solid #999999",
+            borderRadius: "16px",
+            padding: "25px",
+          }}
+        >
+          <h1 style={{ fontSize: "64px", color: "#333333" }}>Preliminary Form</h1>
+          <Flex vertical style={{ width: "100%" }}>
+            <Form
+              form={form}
+              layout="vertical"
+              autoComplete="off"
+            >
+              <Form.Item>
+                <div>
+                  <table style={{ textAlign: "left", width: "100%" }}>
+                    <thead>
+                      <tr>
+                        <th style={{ padding: "0px 11px" }}>MUSCLE/JOINT/BONE</th>
+                        <th style={{ padding: "0px 11px" }}>
+                          EYES/EARS/NOSE/THROAT
+                        </th>
+                        <th style={{ padding: "0px 11px" }}>NEUROLOGIC</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td style={{ padding: "0px 11px" }}>
+                          {[
+                            "Back Pain",
+                            "Leg Pain",
+                            "Neck Pain",
+                            "Arm Pain",
+                            "Joint Pain",
+                          ].map((symptom) => (
+                            <div key={symptom}>
+                              <Checkbox
+                                disabled
+                                checked={
+                                  symptoms["Muscle/Joint/Bone"]?.includes(
+                                    symptom
+                                  ) || false
+                                }
+                                
+                              >
+                                {symptom}
+                              </Checkbox>
+                              <br />
+                            </div>
+                          ))}
+                        </td>
+                        <td style={{ padding: "0px 11px" }}>
+                          {[
+                            "Blurred Vision",
+                            "Loss of Hearing",
+                            "Nose Bleeds",
+                            "Sinus Problems",
+                            "Strep Throat",
+                          ].map((symptom) => (
+                            <div key={symptom}>
+                              <Checkbox
+                                disabled
+                                checked={
+                                  symptoms["Eyes/Ears/Nose/Throat"]?.includes(
+                                    symptom
+                                  ) || false
+                                }
+                                
+                              >
+                                {symptom}
+                              </Checkbox>
+                              <br />
+                            </div>
+                          ))}
+                        </td>
+                        <td style={{ padding: "0px 11px" }}>
+                          {[
+                            "Fainting",
+                            "Dizziness",
+                            "Headache",
+                            "Memory Loss",
+                            "Depression",
+                          ].map((symptom) => (
+                            <div key={symptom}>
+                              <Checkbox
+                                disabled
+                                checked={
+                                  symptoms["Neurologic"]?.includes(symptom) ||
+                                  false
+                                }
+                                
+                              >
+                                {symptom}
+                              </Checkbox>
+                              <br />
+                            </div>
+                          ))}
+                        </td>
+                      </tr>
+                      <tr>
+                        <th style={{ padding: "0px 11px" }}>SKIN</th>
+                        <th style={{ padding: "0px 11px" }}>LUNGS</th>
+                        <th style={{ padding: "0px 11px" }}>CARDIOVASCULAR</th>
+                      </tr>
+                      <tr>
+                        <td style={{ padding: "0px 11px" }}>
+                          {["Itching", "Rash", "Callus"].map((symptom) => (
+                            <div key={symptom}>
+                              <Checkbox
+                                disabled
+                                checked={
+                                  symptoms["Skin"]?.includes(symptom) || false
+                                }
+                                
+                              >
+                                {symptom}
+                              </Checkbox>
+                              <br />
+                            </div>
+                          ))}
+                        </td>
+                        <td style={{ padding: "0px 11px" }}>
+                          {[
+                            "Shortness of Breath",
+                            "Persistent Cough",
+                            "Asthma",
+                            "Sleep Apnea",
+                          ].map((symptom) => (
+                            <div key={symptom}>
+                              <Checkbox
+                                disabled
+                                checked={
+                                  symptoms["Lungs"]?.includes(symptom) || false
+                                }
+                                
+                              >
+                                {symptom}
+                              </Checkbox>
+                              <br />
+                            </div>
+                          ))}
+                        </td>
+                        <td style={{ padding: "0px 11px" }}>
+                          {[
+                            "Chest Pain",
+                            "Irregular Heart Beat",
+                            "Heart Attack",
+                            "Heart Disease",
+                          ].map((symptom) => (
+                            <div key={symptom}>
+                              <Checkbox
+                                disabled
+                                checked={
+                                  symptoms["Cardiovascular"]?.includes(symptom) ||
+                                  false
+                                }
+                                
+                              >
+                                {symptom}
+                              </Checkbox>
+                              <br />
+                            </div>
+                          ))}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </Form.Item>
+            </Form>
+          </Flex>
+        </Flex>
+      </Modal>
+    </>
   );
 };
 
