cba75bcd3017448fb86ae974d9d1fe5654b8a441
AllanRoz <allanrozario22@gmail.com>
2025-04-28 21:31:09 -0400

    Selenium testing and formating for preliminary form

---
+++ selenium-testing/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+# Enter Password
+# password_XPATH = "//*[@id=\"patientsignupform_PW\"]"
+# WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, password_XPATH)))
+# password = driver.find_element(By.XPATH, address_XPATH)
+# password.clear()
+# password.send_keys(fake.password())
