19a61d528e241216b3e8f3dbd11c2afc2b36bdf8
gtj1227 <gtj@njit.edu>
2025-05-06 19:10:49 -0400

    Added AddMedicine.py to populate desired pharmacy drugs

---
+++ selenium-testing/AddMedicine.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+def wait_and_click(driver, by, value, timeout=20):
+    wait = WebDriverWait(driver, timeout)
+    element = wait.until(EC.element_to_be_clickable((by, value)))
+    driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+    element.click()
+    time.sleep(1)
+    return element
+
+service = Service(excutable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+# driver.get("http://localhost:5173")
+driver.get("https://cs490-gp-frontend-production.up.railway.app/") # Open Primewell Cliniic Website
+
+# Login page
+wait_and_click(driver, By.ID, "loginButton")
+
+WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.ID, "pharm-btn")),
+)
+
+wait_and_click(driver, By.ID, "pharm-btn")
+
+WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.ID, "email")),
+)
+
+patient_email = driver.find_element(By.ID, "email")
+patient_email.send_keys("mcoopey2@businesswire.com")
+
+patient_password = driver.find_element(By.ID, "pw")
+patient_password.send_keys('dG8~`SOx/j=N/`Z')
+
+wait_and_click(driver, By.ID, "login-button")
+
+wait_and_click(driver, By.ID, "create-medicine")
+
+wait_and_click(driver, By.ID, "Pill_Name")
+
+pill_name = driver.find_element(By.ID, "Pill_Name")
+pill_name.send_keys("Test Pill")
+
+wait_and_click(driver, By.ID, "Dosage")
+
+pill_type = driver.find_element(By.ID, "Dosage")
+pill_type.send_keys("7")
+
+wait_and_click(driver, By.ID, "Quantity")
+
+pill_description = driver.find_element(By.ID, "Quantity")
+pill_description.send_keys("45")
+
+wait_and_click(driver, By.ID, "Cost")
+
+pill_cost = driver.find_element(By.ID, "Cost")
+pill_cost.send_keys("24.99")
+
+wait_and_click(driver, By.ID, "submit-new-pill")
